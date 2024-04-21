6fbc76529f1f5007a2741794af391550b7cee9f1
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-04-21 21:18:22 +0300

    done-2 on mongodb

--- models/Contact.js
+++ models/Contact.js
@@ -1,22 +1,25 @@
 import { Schema, model } from "mongoose";
 import { handleSaveError, setUpdateSettings } from "./hooks.js";
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favourite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favourite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.post("save", handleSaveError);
 
